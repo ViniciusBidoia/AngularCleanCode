ea719d23e2edc0eecac3194d7bcd19c091731a78
Vinicius Bidoia <vinicius.bidoia@hotmail.com>
2022-06-02 23:52:44 -0300

    removendo modulo do app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PagesModule } from './presentation/view/pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    PagesModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
